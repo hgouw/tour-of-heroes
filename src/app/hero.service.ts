daec1f6fdbcf1d27ab72caf4c0ff4f1e564a2f38
Herman Gouw <hermangouw@gmail.com>
2019-03-22 12:41:01 +1100

    Implemented message service and messages component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // getHeroes() must have an asynchronous signature of some kind
   // 1. a callback or
   // 2. a promise or
   // 3. an observable
   getHeroes(): Observable<Hero[]> { // implemented as an observable
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
