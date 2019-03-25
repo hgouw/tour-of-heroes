dbb2b85d06dce1938aa7e46e7dff4f8d8c5a8436
Herman Gouw <hermangouw@gmail.com>
2019-03-25 14:27:57 +1100

    Simulated a data server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http'; // enable HTTP services
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -15,7 +18,14 @@ import { AppRoutingModule } from './/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   declarations: [
     AppComponent,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -9,7 +10,12 @@ import { MessageService } from './message.service';
   providedIn: 'root' // registered the HeroService at the root level so that it can be injected anywhere in the app
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { } // used Angular Dependency Injection to inject it into another service
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  // used Angular Dependency Injection to inject it into another service
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   // getHeroes() must have an asynchronous signature of some kind
   // 1. a callback or
@@ -18,12 +24,18 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> { // implemented as an observable
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    // return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  // Log a HeroService message with the MessageService
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
