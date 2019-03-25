518e005e10ae2cc9db11df6a025ec4bf9c386e00
Herman Gouw <hermangouw@gmail.com>
2019-03-25 15:33:58 +1100

    Updated getHero by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,9 +34,14 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    // return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /**
